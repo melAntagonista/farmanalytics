13ccb80313fc0b58199e726380a6339b7949b293
Artiom Oganesyan <artiomoganesyan@gmal.com>
2019-06-28 11:07:53 +0300

    first graph work with mongo Count

--- routes/index.js
+++ routes/index.js
@@ -21,98 +21,266 @@ router.route('/gender')
     res.render('gender.hbs')
   })
   .post(async (req, res) => {
-    let patient = await Farm.find({})
-
+    console.log(req.body.gender)
+    let patient;
     let patientCount = 0
     let male = 0
     let female = 0
-
-    if (req.body.age === 'allAge') {
-      for (let i = 0; i < patient.length; i++) {
-        if (patient[i].gender === 'F') {
-          female++
-        } else {
-          male++
-        }
-        patientCount++
-      }
-    } else if (req.body.age === 'age1') {
-      for (let i = 0; i < patient.length; i++) {
-        if (patient[i].age <= 20) {
-          if (patient[i].gender === 'F') {
-            female++
-          } else {
-            male++
-          }
-          patientCount++
-        }
-      }
-    } else if (req.body.age === 'age2') {
-      for (let i = 0; i < patient.length; i++) {
-        if (patient[i].age > 20 && patient[i].age <= 30) {
-          if (patient[i].gender === 'F') {
-            female++
-          } else {
-            male++
-          }
-          patientCount++
-        }
-      }
-    } else if (req.body.age === 'age3') {
-      for (let i = 0; i < patient.length; i++) {
-        if (patient[i].age > 30 && patient[i].age <= 40) {
-          if (patient[i].gender === 'F') {
-            female++
-          } else {
-            male++
-          }
-          patientCount++
-        }
-      }
-    } else if (req.body.age === 'age4') {
-      for (let i = 0; i < patient.length; i++) {
-        if (patient[i].age > 40 && patient[i].age <= 50) {
-          if (patient[i].gender === 'F') {
-            female++
-          } else {
-            male++
-          }
-          patientCount++
-        }
-      }
-    } else if (req.body.age === 'age5') {
-      for (let i = 0; i < patient.length; i++) {
-        if (patient[i].age > 50 && patient[i].age <= 60) {
-          if (patient[i].gender === 'F') {
-            female++
-          } else {
-            male++
-          }
-          patientCount++
-        }
-      }
-    } else if (req.body.age === 'age6') {
-      for (let i = 0; i < patient.length; i++) {
-        if (patient[i].age > 60) {
-          if (patient[i].gender === 'F') {
-            female++
-          } else {
-            male++
-          }
-          patientCount++
-        }
-      }
+    let showMaleGraph = 0
+    let showFemaleGraph = 0
+    let genderAgeCount = {
+      allAge: 0,
+      age1: 0,
+      age2: 0,
+      age3: 0,
+      age4: 0,
+      age5: 0,
+      age6: 0
     }
 
+    if (req.body.gender === 'M') {
+      patient = await Farm.find({
+        gender: 'M'
+      })
+      showMaleGraph++
+      // console.log(patient[343].age)
+      for (let i = 0; i < patient.length; i++) {
+        genderAgeCount.allAge++
+        patientCount++
+
+        if (parseInt(patient[i].age, 10) <= 20) {
+          genderAgeCount.age1++
+          patientCount++
+        } else if (parseInt(patient[i].age, 10) > 20 && parseInt(patient[i].age, 10) <= 30) {
+          genderAgeCount.age2++
+          patientCount++
+        } else if (parseInt(patient[i].age, 10) > 31 && parseInt(patient[i].age, 10) <= 40) {
+          genderAgeCount.age3++
+          patientCount++
+        } else if (parseInt(patient[i].age, 10) > 41 && parseInt(patient[i].age, 10) <= 50) {
+          genderAgeCount.age4++
+          patientCount++
+        } else if (parseInt(patient[i].age, 10) && parseInt(patient[i].age, 10) <= 60) {
+          genderAgeCount.age5++
+          patientCount++
+        } else if (parseInt(patient[i].age, 10) > 61) {
+          genderAgeCount.age6++
+          patientCount++
+        }
+      }
+    } else if (req.body.gender === 'F') {
+      patient = await Farm.find({
+        gender: 'F'
+      })
+      showFemaleGraph++
+      for (let i = 0; i < patient.length; i++) {
+        genderAgeCount.allAge++
+        patientCount++
+
+        if (parseInt(patient[i].age, 10) <= 20) {
+          genderAgeCount.age1++
+          patientCount++
+        } else if (parseInt(patient[i].age, 10) > 20 && parseInt(patient[i].age, 10) <= 30) {
+          genderAgeCount.age2++
+          patientCount++
+        } else if (parseInt(patient[i].age, 10) > 31 && parseInt(patient[i].age, 10) <= 40) {
+          genderAgeCount.age3++
+          patientCount++
+        } else if (parseInt(patient[i].age, 10) > 41 && parseInt(patient[i].age, 10) <= 50) {
+          genderAgeCount.age4++
+          patientCount++
+        } else if (parseInt(patient[i].age, 10) && parseInt(patient[i].age, 10) <= 60) {
+          genderAgeCount.age5++
+          patientCount++
+        } else if (parseInt(patient[i].age, 10) > 61) {
+          genderAgeCount.age6++
+          patientCount++
+        }
+      }
+    } else {
+      if (req.body.gender === 'allSex') {
+        if (req.body.age === 'allAge') {
+          female = await Farm.count({
+            gender: 'F'
+          })
+          male = await Farm.count({
+            gender: 'M'
+          })
+        } else if (req.body.age === 'age1') {
+          male = await Farm.find({
+            age: {
+              $lt: '20'
+            },
+            gender: 'F'
+          }).count()
+          female = await Farm.find({
+            age: {
+              $lt: '20'
+            },
+            gender: 'M'
+          }).count()
+        } else if (req.body.age === 'age2') {
+          female = await Farm.count({
+            age: {
+              $gt: '21',
+              $lt: '30'
+            },
+            gender: 'F'
+          })
+          male = await Farm.count({
+            age: {
+              $gt: '21',
+              $lt: '30'
+            },
+            gender: 'M'
+          })
+        } else if (req.body.age === 'age3') {
+          male = await Farm.find({
+            age: {
+              $gt: '31',
+              $lt: '40'
+            },
+            gender: 'F'
+          }).count()
+          female = await Farm.find({
+            age: {
+              $gt: '31',
+              $lt: '40'
+            },
+            gender: 'M'
+          }).count()
+        } else if (req.body.age === 'age4') {
+          male = await Farm.find({
+            age: {
+              $gt: '31',
+              $lt: '40'
+            },
+            gender: 'F'
+          }).count()
+          female = await Farm.find({
+            age: {
+              $gt: '31',
+              $lt: '40'
+            },
+            gender: 'M'
+          }).count()
+        } else if (req.body.age === 'age3') {
+          male = await Farm.find({
+            age: {
+              $gt: '31',
+              $lt: '40'
+            },
+            gender: 'F'
+          }).count()
+          female = await Farm.find({
+            age: {
+              $gt: '31',
+              $lt: '40'
+            },
+            gender: 'M'
+          }).count()
+        } else if (req.body.age === 'age3') {
+          male = await Farm.find({
+            age: {
+              $gt: '31',
+              $lt: '40'
+            },
+            gender: 'F'
+          }).count()
+          female = await Farm.find({
+            age: {
+              $gt: '31',
+              $lt: '40'
+            },
+            gender: 'M'
+          }).count()
+        }
+
+        // else if (req.body.age === 'age1') {
+        //   for (let i = 0; i < patient.length; i++) {
+        //     if (patient[i].age <= 20) {
+        //       if (patient[i].gender === 'F') {
+        //         female++
+        //       } else {
+        //         male++
+        //       }
+        //       patientCount++
+        //     }
+        //   }
+        // } else if (req.body.age === 'age2') {
+        //   for (let i = 0; i < patient.length; i++) {
+        //     if (patient[i].age > 20 && patient[i].age <= 30) {
+        //       if (patient[i].gender === 'F') {
+        //         female++
+        //       } else {
+        //         male++
+        //       }
+        //       patientCount++
+        //     }
+        //   }
+        // } else if (req.body.age === 'age3') {
+        //   for (let i = 0; i < patient.length; i++) {
+        //     if (patient[i].age > 30 && patient[i].age <= 40) {
+        //       if (patient[i].gender === 'F') {
+        //         female++
+        //       } else {
+        //         male++
+        //       }
+        //       patientCount++
+        //     }
+        //   }
+        // } else if (req.body.age === 'age4') {
+        //   for (let i = 0; i < patient.length; i++) {
+        //     if (patient[i].age > 40 && patient[i].age <= 50) {
+        //       if (patient[i].gender === 'F') {
+        //         female++
+        //       } else {
+        //         male++
+        //       }
+        //       patientCount++
+        //     }
+        //   }
+        // } else if (req.body.age === 'age5') {
+        //   for (let i = 0; i < patient.length; i++) {
+        //     if (patient[i].age > 50 && patient[i].age <= 60) {
+        //       if (patient[i].gender === 'F') {
+        //         female++
+        //       } else {
+        //         male++
+        //       }
+        //       patientCount++
+        //     }
+        //   }
+        // } else if (req.body.age === 'age6') {
+        //   for (let i = 0; i < patient.length; i++) {
+        //     if (patient[i].age > 60) {
+        //       if (patient[i].gender === 'F') {
+        //         female++
+        //       } else {
+        //         male++
+        //       }
+        //       patientCount++
+        //     }
+        //   }
+        // }
+      }
+
+    }
+    // console.log('!!!')
+    console.log(female, male)
     let gender = {
       m: male,
       f: female,
-      all: patientCount
+      all: 0
     }
-
+    console.log(gender)
     res.render('gender.hbs', {
-      gender
+      gender,
+      showMaleGraph,
+      showFemaleGraph,
+      genderAgeCount
     })
+
   })
 
 
@@ -195,7 +363,8 @@ router.route('/login')
       f: female,
       all: patientCount
     }
-
+    console.log(genderAgeCount)
+    console.log(gender)
     // console.log(patient)
     res.render('login', {
       patient,
